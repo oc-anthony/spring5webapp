39d8152a4d6e40c812374826f84e0779c11b8036
oc-anthony <oc.anthony.ii@gmail.com>
2021-04-14 02:12:58 +0800

    Add a Book Controller

---
+++ src/main/java/genesys/sf/demo/webapp/controllers/BookController.java
@@ -0,0 +1,25 @@
+package genesys.sf.demo.webapp.controllers;
+
+import genesys.sf.demo.webapp.model.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        String viewName = "books";
+        return viewName;
+    }
+}
