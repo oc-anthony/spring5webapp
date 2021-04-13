782b695720c7ec98a7ea9fc70960408f1271e768
oc-anthony <oc.anthony.ii@gmail.com>
2021-04-13 22:51:48 +0800

    Add a POJO - Book

---
+++ src/main/java/genesys/sf/demo/webapp/model/Book.java
@@ -0,0 +1,41 @@
+package genesys.sf.demo.webapp.model;
+
+public class Book {
+
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
