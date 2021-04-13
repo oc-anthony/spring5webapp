7fe16aea9123545fdc4921401bf87561126efc33
oc-anthony <oc.anthony.ii@gmail.com>
2021-04-13 22:31:00 +0800

    Initial commit for a sample Spring Boot App

---
+++ src/test/java/genesys/sf/demo/webapp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package genesys.sf.demo.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
