e78be7d5a928dd5b96e07dc92cc3a46c5589c906
oc-anthony <oc.anthony.ii@gmail.com>
2021-04-14 00:00:29 +0800

    Convert Publisher to Entity

--- src/main/java/genesys/sf/demo/webapp/model/Publisher.java
+++ src/main/java/genesys/sf/demo/webapp/model/Publisher.java
@@ -1,7 +1,16 @@
 package genesys.sf.demo.webapp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Publisher {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String city;
     private String state;
@@ -13,6 +22,14 @@ public class Publisher {
         this.name = name;
         this.city = city;
         this.state = state;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
