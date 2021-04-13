07c3c5f413e535b3974f27d15ea6044ab58e2e00
oc-anthony <oc.anthony.ii@gmail.com>
2021-04-14 03:10:28 +0800

    Add an Author Controller

---
+++ src/main/java/genesys/sf/demo/webapp/controllers/AuthorController.java
@@ -0,0 +1,25 @@
+package genesys.sf.demo.webapp.controllers;
+
+import genesys.sf.demo.webapp.model.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        String viewName = "authors";
+        return viewName;
+    }
+}
