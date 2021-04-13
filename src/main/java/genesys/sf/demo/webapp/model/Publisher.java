5ba2b287f2886ed000d6511b6852683daf3c80d7
oc-anthony <oc.anthony.ii@gmail.com>
2021-04-14 00:12:31 +0800

    Implement equals, hashCode, toString in Publisher

--- src/main/java/genesys/sf/demo/webapp/model/Publisher.java
+++ src/main/java/genesys/sf/demo/webapp/model/Publisher.java
@@ -2,6 +2,7 @@ package genesys.sf.demo.webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -57,5 +58,31 @@ public class Publisher {
 
     public void setState(String state) {
         this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
